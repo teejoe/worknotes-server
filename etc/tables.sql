a2eb5b593a4a3e0e84ee54a5addd883ec597a8df
teejoe <mtjm2x@gmail.com>
2019-02-25 10:20:02 +0800

    fix time unit

--- etc/tables.sql
+++ etc/tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `worknotes` (
   `category` varchar(50) DEFAULT NULL,
   `content` varchar(255) DEFAULT NULL,
   `cost` decimal(4,2) DEFAULT 0,
-  `time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `uname` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
